15fe306ebabf319a07eede8b7ad22dcd55f264a0
Wilczak3 <70924433+Wilczak3@users.noreply.github.com>
2020-09-08 14:39:15 -0400

    Add files via upload

---
+++ ClassRelationships/Test.java
@@ -0,0 +1,11 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Rectangle r1 = new Rectangle(5,5);
+        System.out.println(r1.getArea());
+        System.out.println(r1.getParameter());
+        System.out.println(r1.getHeight());
+        System.out.println(r1.getWidth());
+    }
+}
