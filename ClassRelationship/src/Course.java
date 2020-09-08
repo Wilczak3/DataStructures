015446e55432d0a8c5aa61a6ea36dbae40e971c5
Wilczak3 <70924433+Wilczak3@users.noreply.github.com>
2020-09-08 15:17:23 -0400

    Add files via upload

---
+++ ClassRelationship/src/Course.java
@@ -0,0 +1,33 @@
+public class Course
+{
+    private String m_title;
+    private String m_start;
+    private String m_end;
+
+    public Course(String title, String start, String end)
+    {
+        m_title = title;
+        m_start = start;
+        m_end = end;
+    }
+
+    public String getTitle()
+    {
+        return m_title;
+    }
+
+    public String getStart()
+    {
+        return m_start;
+    }
+
+    public String getEnd()
+    {
+        return m_end;
+    }
+    @Override
+    public String toString()
+    {
+        return this.m_title + ": " + this.m_start + " - " + this.m_end + "\n";
+    }
+}
