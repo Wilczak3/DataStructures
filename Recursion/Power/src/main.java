21f2f9e5c51195bfe0d7d26debc79ffb70e6a354
Wilczak3 <70924433+Wilczak3@users.noreply.github.com>
2020-09-24 20:36:11 -0400

    Add files via upload

---
+++ Recursion/Power/src/main.java
@@ -0,0 +1,12 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        Recursively r = new Recursively();
+
+        int n = 4;
+        int p = 4;
+
+        System.out.println(r.power(n, p));
+    }
+}
